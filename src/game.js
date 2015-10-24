0f2bea304e4528324c6dc1ee3d335a2ecab138a2
Dean <dcheesman@gmail.com>
2015-10-24 12:24:49 -0600

    fixing tabs/spaces

--- src/game.js
+++ src/game.js
@@ -6,39 +6,39 @@ stars = [];
 var game_state;
 
 function setup() {
-	socket.emit('game:control');
+    socket.emit('game:control');
 
-  socket.on('pregame', function( countdown ){
-    background(100); // Clears Background
-    textAlign(CENTER);
-    noStroke();
-    textStyle(ITALIC)
-    textSize(30);
-    text("JOIN THE NEXT ROUND", width/2, height/2 - 100);
-    text("POINT YOUR MOBILE BROWSER TO:", width/2, height/2 - 50);
-    textSize(50);
-    text("192.168.1.6", width/2, height/2);
-    textSize(70);
-    text(countdown, width/2, height/2 + 70);
-  });
+    socket.on('pregame', function( countdown ){
+        background(100); // Clears Background
+        textAlign(CENTER);
+        noStroke();
+        textStyle(ITALIC)
+        textSize(30);
+        text("JOIN THE NEXT ROUND", width/2, height/2 - 100);
+        text("POINT YOUR MOBILE BROWSER TO:", width/2, height/2 - 50);
+        textSize(50);
+        text("192.168.1.6", width/2, height/2);
+        textSize(70);
+        text(countdown, width/2, height/2 + 70);
+    });
 
-  socket.on('postgame', function( winner ){
-    background(100); // Clears Background
-    textAlign(CENTER);
-    noStroke();
-    textStyle(ITALIC)
-    textSize(30);
-    text("GAME OVER", width/2, height/2 - 50);
-    textSize(50);
-    text(winner + " WINS!", width/2, height/2);
-    textSize(70);
-  });
+    socket.on('postgame', function( winner ){
+        background(100); // Clears Background
+        textAlign(CENTER);
+        noStroke();
+        textStyle(ITALIC)
+        textSize(30);
+        text("GAME OVER", width/2, height/2 - 50);
+        textSize(50);
+        text(winner + " WINS!", width/2, height/2);
+        textSize(70);
+    });
 
-  socket.on('game:state', function(_game_state){
-    console.log('ships: ' + game_state);
-    game_state = _game_state;
-    draw_game();
-  });
+    socket.on('game:state', function(_game_state){
+        console.log('ships: ' + game_state);
+        game_state = _game_state;
+        draw_game();
+    });
 
   socket.emit('display');
 
@@ -89,13 +89,13 @@ function drawStarField(num) {
 };
 
 function draw_game() {
-  // background('CURRENT MAP');
-  imageMode(CENTER);
-  ships.forEach( function (ship) {
-    push();
-    translate(ship.x, ship.y);
-    rotate(ship.r);
-    image(ship.image,0,0);
-    pop();
-  });
+    // background('CURRENT MAP');
+    imageMode(CENTER);
+    ships.forEach( function (ship) {
+        push();
+        translate(ship.x, ship.y);
+        rotate(ship.r);
+        image(ship.image,0,0);
+        pop();
+    });
 }
