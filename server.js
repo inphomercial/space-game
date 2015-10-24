bc9cd2e4a79b2f623035534460c9c1ee6fa75b87
Marcos Minond <minond.marcos@gmail.com>
2015-10-24 14:12:43 -0600

    assets static dir in server

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ app.set('view cache', DEBUG);
 
 app.use('/node_modules', express.static('node_modules'));
 app.use('/vendor', express.static('vendor'));
+app.use('/assets', express.static('assets'));
 app.use('/src', express.static('src'));
 app.get('/', function (req, res) { res.sendfile('client.html'); });
 app.get('/monitor', function (req, res) { res.sendfile('monitor.html'); });
