e189ef78018e15e03df4c5553a4b97388946a3e0
Dean <dcheesman@gmail.com>
2015-10-23 23:05:14 -0600

    added client bg color

--- src/client.js
+++ src/client.js
@@ -2,14 +2,19 @@
 
 var socket;
 
+var bg = color(255);
+
 function setup() {
     socket = io();
+    createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+    background(bg);
     var rotation_speed = pRotationZ - rotationZ;
 
     if (rotation_speed != 0) {
         socket.emit('turn', rotation_speed);
     }
-}
+
+socket.on('color', funtion(d) { color = color(d);});
